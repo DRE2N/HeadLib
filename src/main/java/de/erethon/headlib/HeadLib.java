39cc1af6683bf0646e01eb90f89355ffed54818d
Daniel Saukel <sa.da@unitybox.de>
2018-08-26 22:00:07 +0200

    Update to 1.13.1

--- src/main/java/de/erethon/headlib/HeadLib.java
+++ src/main/java/de/erethon/headlib/HeadLib.java
@@ -15,12 +15,12 @@ package de.erethon.headlib;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import net.minecraft.server.v1_13_R1.NBTTagCompound;
-import net.minecraft.server.v1_13_R1.NBTTagList;
-import net.minecraft.server.v1_13_R1.NBTTagString;
+import net.minecraft.server.v1_13_R2.NBTTagCompound;
+import net.minecraft.server.v1_13_R2.NBTTagList;
+import net.minecraft.server.v1_13_R2.NBTTagString;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.craftbukkit.v1_13_R1.inventory.CraftItemStack;
+import org.bukkit.craftbukkit.v1_13_R2.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -462,7 +462,7 @@ public enum HeadLib {
             item.setItemMeta(meta);
         }
 
-        net.minecraft.server.v1_13_R1.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
+        net.minecraft.server.v1_13_R2.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
         NBTTagCompound compound = nmsStack.getTag() != null ? nmsStack.getTag() : new NBTTagCompound();
         compound.set("SkullOwner", skullOwner);
         nmsStack.setTag(compound);
